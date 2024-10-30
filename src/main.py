4e5f10a460daffc8ab5ca64abdf96fe7823606ae
C-Vac <c.vaccarogallo@gmail.com>
2024-10-30 09:43:56 -0400

    gitignore etc

--- src/main.py
+++ src/main.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .database import nuke_database, push_model_updates
 from .routers import auth, post, user, vote
 
 app = FastAPI()
 
+origins = []  # CORS origins
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # update mode
 # nuke_database()
